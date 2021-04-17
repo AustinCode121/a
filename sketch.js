b29aef54b503c8a1ad6402642e438257ac422144
AustinCode121 <73318569+AustinCode121@users.noreply.github.com>
2021-04-16 22:02:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var player;
+function setup() {
+  createCanvas(1080,1920);
+  player = createSprite(200,200,30,30);
+}
+function preload(){
+ 
+ 
+}
+
+function draw() {
+ background("black")
+  
+drawSprites();
+}
